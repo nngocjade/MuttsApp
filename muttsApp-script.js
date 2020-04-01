8d6920b9a4a8bb752c55ae13b307b965f2e1feac
nngocjade <nngocjade@gmail.com>
2020-04-02 05:56:20 +0800

    in progress

--- muttsApp-script.js
+++ muttsApp-script.js
@@ -17,41 +17,65 @@ newMessageForm.addEventListener("submit", function(event) {
   //pass a callback function, submission of form occurs then the function is called
   event.preventDefault(); //overrides html form submission, IMPORTANT!
   let msg = document.getElementById("new-message").value; //storing the inputted value by user into "msg"
-  createChatBubble(msg); //passing "msg" into "createChatBubble" function
+  createChatBubble(msg);
+  //passing "msg" into "createChatBubble" function
   document.getElementById("new-message").value = " "; //deleting (clearing) value in the input, because it is now on the screen/in chat
 });
+
+//As soon as JS file loads, we run this function to get all the items for the sidebar
+function getUserChats() {
+  fetch("http://localhost:8080/muttsApp.html")
+    .then(Response => Response.json())
+    .then(dataObj => createMessagePreviewBox(dataObj));
+}
+
+function createPreviewBoxes(dataObj) {
+  let chatsArr = dataObj.data;
+  chatsArr.forEach(chatObj => createMessagePreviewBox(chatObj));
+}
+
+getUserChats();
 
 function createMessagePreviewBox(chatObj) {
   let MessagePreviewBox = document.createElement("div");
   MessagePreviewBox.classList.add("message-preview-box");
+  MessagePreviewBox.setAttribute("data-chat_id", chatObj.sender_id);
+  MessagePreviewBox.addEventListener("click", previewBoxClick);
 
   let imgWrap = document.createElement("div");
+  imgWrap.setAttribute("data-chat_id", chatObj.sender_id);
   imgWrap.classList.add("img-wrap");
-  let img = new Image();
-  img.src = chatObj.img;
-  img.alt = chatObj.alt;
+  let image = document.createElement("img");
+  image.setAttribute("data-chat_id", chatObj.sender_id);
+  image.setAttribute("src", chatObj.photo_url);
+  image.setAttribute("alt", "default icon");
 
-  imgWrap.appendChild(img);
+  imgWrap.appendChild(image);
 
-  let messageTextWrap = document.createElement("div");
-  messageTextWrap.classList.add("message-text-wrap");
-  let SenderNameParagraph = document.createElement("p");
-  SenderNameParagraph.innerText = chatObj.name;
-  let SenderMessageParagraph = document.createElement("p");
-  SenderMessageParagraph.innerText = chatObj.msg;
+  let textWrap = document.createElement("div");
+  textWrap.setAttribute("data-chat_id", chatObj.sender_id);
+  textWrap.classList.add("message-text-wrap");
+  let nameParagraph = document.createElement("p");
+  nameParagraph.setAttribute("chat-chat_id", chatObj.sender_id);
+  nameParagraph.innerText = chatObj.chat_name;
+  let messageParagraph = document.createElement("p");
+  messageParagraph.setAttribute("chat-chat_id", chatObj.sender_id);
+  messageParagraph.innerText = chatObj.last_message;
 
-  messageTextWrap.appendChild(SenderNameParagraph);
-  messageTextWrap.appendChild(SenderMessageParagraph);
+  textWrap.appendChild(nameParagraph);
+  textWrap.appendChild(messageParagraph);
 
   let dateWrap = document.createElement("div");
+  dateWrap.setAttribute("data-chat_id", chatObj.sender_id);
   dateWrap.classList.add("date-wrap");
-  let dateParagraph = document.createElement("p");
-  dateParagraph.innerHTML = new Date(chatObj.date).toLocaleDateString(); //always instantiate a new date
+  let dateP = document.createElement("p");
+  dateP.setAttribute("data-chat_id", chatObj.sender_id);
+  dateP.innerHTML = new Date(chatObj.date_sent).toLocaleDateString(); //always instantiate a new date
 
-  dateWrap.appendChild(dateParagraph);
+  dateWrap.appendChild(dateP);
 
   MessagePreviewBox.appendChild(imgWrap);
-  MessagePreviewBox.appendChild(messageTextWrap);
+  MessagePreviewBox.appendChild(textWrap);
   MessagePreviewBox.appendChild(dateWrap);
 
   let MessagePreviewWrapper = document.getElementById(
@@ -60,48 +84,63 @@ function createMessagePreviewBox(chatObj) {
   MessagePreviewWrapper.appendChild(MessagePreviewBox);
 }
 
+function previewBoxClick(event) {
+  // This gets the value of the "data-chat_id" attribute on the clicked element
+  let chatID = event.target.dataset.chat_id;
+  //The value of "chatID" is passed to this url, to create a dynamically generated API based on which preview box is clicked
+  fetch("http://localhost:8080/muttsApp.html")
+    //The info retrieved in the fetch request returns a response object.
+    //The response object is assigned to the parameter in the following method as "response"
+    .then(ressponse => ressponse.json())
+    //The response object needs to be turned into a JS object for parsing. That process is above, then the result is passed to the next '.then' method
+
+    // The object created in the last step is assigned to "dataObj", then the data object is passed to a function that handles the creation of a chat message bubble
+    .then(dataObj => createChatBubbles(dataObj));
+}
+
+//******* SAVED FOR LATER!! *******
 let chats = [
   {
-    img: "./images/icons8-bullbasaur-50.png",
-    alt: "bullbasaur",
-    msg: "hi",
-    name: "Bullbasaur",
-    date: "3/12/20"
+    sender_id: "1",
+    photo_url: "./images/icons8-bullbasaur-50.png",
+    last_message: "hi",
+    chat_name: "Bullbasaur",
+    date_sent: "3/19/20"
   },
   {
-    img: "./images/icons8-pikachu-pokemon-50.png",
-    alt: "pikachu",
-    msg: "hello",
-    name: "pikachu",
-    date: "3/19/20"
+    sender_id: "2",
+    photo_url: "./images/icons8-pikachu-pokemon-50.png",
+    last_message: "hello",
+    chat_name: "pikachu",
+    date_sent: "3/19/20"
   },
   {
-    img: "./images/icons8-charmander-50.png",
-    alt: "charmander",
-    msg: "How it's going?",
-    name: "Charmander",
-    date: "3/20/20"
+    sender_id: "3",
+    photo_url: "./images/icons8-charmander-50.png",
+    last_message: "How it's going?",
+    chat_name: "Charmander",
+    date_sent: "3/20/20"
   },
   {
-    img: "./images/icons8-eevee-50.png",
-    alt: "eevee",
-    msg: "Hey girl!",
-    name: "Eevee",
-    date: "3/21/20"
+    sender_id: "4",
+    photo_url: "./images/icons8-eevee-50.png",
+    last_message: "Hey girl!",
+    chat_name: "Eevee",
+    date_sent: "3/21/20"
   },
   {
-    img: "./images/icons8-jigglypuff-50.png",
-    alt: "jigglypuff",
-    msg: "Wanna hangout?",
-    name: "Jiggly Puff",
-    date: "3/22/20"
+    sender_id: "5",
+    photo_url: "./images/icons8-jigglypuff-50.png",
+    last_message: "Wanna hangout?",
+    chat_name: "Jiggly Puff",
+    date_sent: "3/22/20"
   },
   {
-    img: "./images/icons8-dratini-50.png",
-    alt: "dratini",
-    msg: "See ya later!",
-    name: "Dratini",
-    date: "3/23/20"
+    sender_id: "6",
+    photo_url: "./images/icons8-dratini-50.png",
+    last_message: "See ya later!",
+    chat_name: "Dratini",
+    date_sent: "3/23/20"
   }
 ];
 
