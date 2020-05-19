e726c95a6f251afb291f36c89b62a4b2bfc4bb58
nngocjade <nngocjade@gmail.com>
2020-05-19 11:06:17 +0800

    repositioned emoji container

--- js/muttsApp-script.js
+++ js/muttsApp-script.js
@@ -237,37 +237,6 @@ function openDropdown2(event) {
   document.getElementById("dropdown-content-main").classList.toggle("show");
 }
 
-// window.addEventListener("click", function (event) {
-//   if (!event.target.matches(".dropdown-button")) {
-//     var dropdowns = document.getElementsByClassName("dropdown-content");
-//     var i;
-//     for (i = 0; i < dropdowns.length; i++) {
-//       var openDropdown = dropdowns[i];
-//       if (openDropdown.classList.contains("show")) {
-//         openDropdown.classList.remove("show");
-//       }
-//     }
-//   }
-// });
-
-// --------------------EMOJI BUTTON--------------------------------
-
-// let emojiButton = document.getElementById("emoji-button");
-// console.log($("#new-message"));
-
-// $(document).ready(function (e) {
-//   $("#new-message").emojiPicker({
-//     height: "300px",
-//     width: "450px",
-//     button: false,
-//   });
-
-//   $("#emoji-button").click(function (event) {
-//     event.preventDefault();
-//     $("#new-message").emojiPicker("toggle");
-//   });
-// });
-
 //--------------------   ADD EVENT(SUBMIT) LISTENER    --------------------//
 
 let newMessageForm = document.getElementById("send-message");
@@ -369,25 +338,84 @@ function newUser() {
 // }
 
 window.addEventListener("DOMContentLoaded", () => {
-  02;
   const button = document.querySelector("#emoji-button");
-  03;
+
   const picker = new EmojiButton();
-  04;
 
-  05;
   picker.on("emoji", (emoji) => {
-    06;
     document.querySelector("#new-message").value += emoji;
-    07;
   });
-  08;
 
-  09;
   button.addEventListener("click", () => {
-    10;
     picker.pickerVisible ? picker.hidePicker() : picker.showPicker(button);
-    11;
   });
-  12;
 });
+
+// const picker = new EmojiButton({
+//   // position of the emoji picker. Available positions:
+//   // auto-start, auto-end, top, top-start, top-end, right, right-start, right-end, bottom, bottom-start, bottom-end, left, left-start, left-end
+//   position: auto - end,
+
+//   // 1.0, 2.0, 3,0, 4.0, 5.0, 11.0, 12.0, 12.1
+//   emojiVersion: "12.1",
+
+//   // auto close the emoji picker after selection
+//   autoHide: true,
+
+//   // auto move focus to search field or not
+//   autoFocusSearch: true,
+
+//   // show the emoji preview
+//   showPreview: true,
+
+//   // show the emoji search input
+//   showSearch: true,
+
+//   // show recent emoji
+//   showRecents: true,
+
+//   // show skin tone variants
+//   showVariants: true,
+
+//   // or 'twemoji'
+//   style: "native",
+
+//   // 'light', 'drak', or 'auto'
+//   theme: "drak",
+
+//   // maximum number of recent emojis to save
+//   recentsCount: 50,
+
+//   // z-index property
+//   zIndex: 999,
+
+//   // an array of the categories to show
+//   categories: [
+//     "smileys",
+//     "people",
+//     "animals",
+//     "food",
+//     "activities",
+//     "travel",
+//     "objects",
+//     "symbols",
+//     "flags",
+//   ],
+
+//   // i18n
+//   i18n: {
+//     search: "Search",
+//     categories: {
+//       recents: "Recently Used",
+//       smileys: "Smileys & People",
+//       animals: "Animals & Nature",
+//       food: "Food & Drink",
+//       activities: "Activities",
+//       travel: "Travel & Places",
+//       objects: "Objects",
+//       symbols: "Symbols",
+//       flags: "Flags",
+//     },
+//     notFound: "No emojis found",
+//   },
+// });
